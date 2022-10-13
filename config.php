d935ee7df23d248e61462ec056d99f1b41306aaf
camila <emmaandrea2002@gmail.com>
2022-10-13 09:16:26 -0500

    se agrego nueva cofiguracion

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 
 return [
   'db' => [
-    'host' => 'localhost',
-    'user' => 'root',
-    'pass' => '',
-    'name' => 'tutorial_crud',
+    'host' => 'bauxe11pjzunmyjk9jjg-mysql.services.clever-cloud.com',
+    'user' => 'uywafq3so4jcmemu',
+    'pass' => 'KuhHY0heOwhbTBYYgjjV',
+    'name' => 'bauxe11pjzunmyjk9jjg',
     'options' => [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]
